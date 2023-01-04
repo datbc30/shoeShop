48966686a4066bdad6bf8699e5c2a8deb336bdcc
datbc30 <vuthanhdat297@gmail.com>
2023-01-04 10:49:23 +0700

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  unstable_HistoryRouter as HistoryRouter,
+} from "react-router-dom";
+import Home from './pages/HomePage/Home';
+import { store } from './redux/configStore';
+import HomeTemplate from './template/HomeTemplate';
+import '../src/assets/scss/style.scss'
 
+
+export const history = createBrowserHistory({ window });
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route path="" element={<HomeTemplate />}>
+          <Route path="" element={<Home />}></Route>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </HistoryRouter>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
